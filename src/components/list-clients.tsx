c53082589406d9db8170b1b0204ca5e33d3d4aa4
Maccuci <antoniogabrielleal@gmail.com>
2023-05-18 02:22:02 -0300

    created client-item and display notification when app starts

--- src/components/list-clients.tsx
+++ src/components/list-clients.tsx
@@ -1,10 +1,8 @@
-"use client";
-
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import ClientItem from "./client-item";
 
 const ClientList = () => {
-    //Clientes fictícios, em breve a adição do banco de dados
     const clients = [
         { id: 1, name: "Cliente 1", description: "Tosa - Animal Grande" },
         { id: 2, name: "Cliente 2", description: "Tosa - Animal Pequeno" },
@@ -30,7 +28,7 @@ const ClientList = () => {
     };
 
     return (
-        <div className="bg-gray-200 p-4">
+        <div className="bg-gray-200 p-4 min-h-screen flex flex-col">
             <div className="flex justify-between">
                 <h1 className="text-2xl">Lista de Clientes</h1>
                 <Link
@@ -41,21 +39,30 @@ const ClientList = () => {
                 </Link>
             </div>
 
-            <ul className="grid grid-cols-2 gap-4">
+            <ul className="grid grid-cols-2 gap-4 flex-grow">
                 {currentClients.map((client) => (
-                    <li key={client.id} className="text-lg py-2">
-                        <p>{client.name}</p>
-                        <p>{client.description}</p>
-                    </li>
+                    <ClientItem
+                        key={client.id}
+                        className="text-lg py-2"
+                        id={client.id}
+                        name={client.name}
+                        description={client.description}
+                    />
                 ))}
             </ul>
 
             <div className="flex justify-center mt-24">
-                {Array.from({ length: Math.ceil(clients.length / itemsPerPage) }, (_, index) => index + 1).map((page) => (
+                {Array.from(
+                    { length: Math.ceil(clients.length / itemsPerPage) },
+                    (_, index) => index + 1
+                ).map((page) => (
                     <button
                         key={page}
                         onClick={() => changePage(page)}
-                        className={`px-4 py-2 mx-1 rounded ${currentPage === page ? "bg-blue-500 text-white" : "bg-gray-300 text-gray-800"}`}
+                        className={`px-4 py-2 mx-1 rounded ${currentPage === page
+                                ? "bg-blue-500 text-white"
+                                : "bg-gray-300 text-gray-800"
+                            }`}
                     >
                         {page}
                     </button>
