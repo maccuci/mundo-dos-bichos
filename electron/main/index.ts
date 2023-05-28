269630ed3b07e3247d5d89e68ccbcfa33323785a
Maccuci <antoniogabrielleal@gmail.com>
2023-05-28 04:50:39 -0300

    edit index.ts

--- electron/main/index.ts
+++ electron/main/index.ts
@@ -21,21 +21,15 @@ process.env.PUBLIC = process.env.VITE_DEV_SERVER_URL
     ? join(process.env.DIST_ELECTRON, '../public')
     : process.env.DIST
 
-// Disable GPU Acceleration for Windows 7
 if (release().startsWith('6.1')) app.disableHardwareAcceleration()
 
 // Set application name for Windows 10+ notifications
-if (process.platform === 'win32') app.setAppUserModelId(app.getName())
+if (process.platform === 'win32') app.setAppUserModelId("Mundo dos Bichos")
 
 if (!app.requestSingleInstanceLock()) {
     app.quit()
     process.exit(0)
 }
-
-// Remove electron security warnings
-// This warning only shows in development mode
-// Read more on https://www.electronjs.org/docs/latest/tutorial/security
-// process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true'
 
 let win: BrowserWindow | null = null
 // Here, you can also use other preload
