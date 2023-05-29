637bf9fc11a1a8fb4b6be9ed35dcd548c5d88aae
Maccuci <antoniogabrielleal@gmail.com>
2023-05-29 00:19:32 -0300

    edit server.ts

--- express/server.ts
+++ express/server.ts
@@ -8,30 +8,33 @@ export const expressApp = express();
 process.env.DIST_ELECTRON = join(__dirname, '../')
 process.env.DIST = join(process.env.DIST_ELECTRON, '../dist')
 
-expressApp.get("/api/customers", (req, res) => {
-  const query = "SELECT * FROM customers";
-  const connection = getConnection()
-  connection.query(query, (err, results) => {
-    if (err) {
-      console.error("Ocorreu um erro ao buscar os clientes:", err);
-      res.status(500).json({ error: "Erro ao buscar clientes" });
-    } else {
-      res.json(results);
-    }
-  });
-});
-
-expressApp.get("/api/schedules", (req, res) => {
-    const query = "SELECT * FROM schedules";
-    const connection = getConnection()
-    connection.query(query, (err, results) => {
-        if (err) {
-            console.error("Ocorreu um erro ao buscar os agendamentos:", err);
-            res.status(500).json({ error: "Erro ao buscar agendamentos" });
-        } else {
-            res.json(results);
-        }
+const fetchApis = () => {
+    expressApp.get("/api/customers", (req, res) => {
+        const query = "SELECT * FROM customers";
+        const connection = getConnection()
+        connection.query(query, (err, results) => {
+            if (err) {
+                console.error("Ocorreu um erro ao buscar os clientes:", err);
+                res.status(500).json({ error: "Erro ao buscar clientes" });
+            } else {
+                res.json(results);
+            }
+        });
     });
-});
 
+    expressApp.get("/api/schedules", (req, res) => {
+        const query = "SELECT * FROM schedules";
+        const connection = getConnection()
+        connection.query(query, (err, results) => {
+            if (err) {
+                console.error("Ocorreu um erro ao buscar os agendamentos:", err);
+                res.status(500).json({ error: "Erro ao buscar agendamentos" });
+            } else {
+                res.json(results);
+            }
+        });
+    });
+}
+
+fetchApis();
 expressApp.use(express.static(process.env.DIST));
